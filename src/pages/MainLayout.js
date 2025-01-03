81081c0e0162cdf3eed92ef47f2dc7be41e6a1fd
jabbar <jaffarifnuadam@outlook.com>
2025-01-03 11:28:06 +0530

    new

--- src/pages/MainLayout.js
+++ src/pages/MainLayout.js
@@ -1,9 +1,6 @@
 // src/layouts/MainLayout.js
 import React from 'react';
 import Navbar from '../components/Navbar';
-import Home from './Home';
-import Photos from './Photos';
-import About from './About';
 import Footer from '../components/Footer';
 
 const MainLayout = ({ children }) => {
